0869b2e3e7e711894fdd2e31d6960553bf990201
manalinjadhav <manalinjadhav1998@gmail.com>
2025-10-04 17:45:12 +0530

    base updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/weather-now/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
